17a08abcf54f47787684a2708095d107f13ac9ac
salonikakriye <vanshukakriye4@gmail.com>
2026-07-22 16:29:35 +0530

    day1

--- day1/resource.tf
+++ day1/resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
     ami = "ami-0b1ed96948adabcd9"
     instance_type = "t3.micro"
-    key_name = "my-key"
+    key_name = "key"
     tags = {
         Name = "server"
     }
