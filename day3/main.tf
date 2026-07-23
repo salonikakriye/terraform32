9ef2b9a796948ebc79d206059a812b72baeaf0fb
salonikakriye <vanshukakriye4@gmail.com>
2026-07-23 16:33:17 +0530

    day3

---
+++ day3/main.tf
@@ -0,0 +1,124 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = var.vpc_cidr
+    tags = {
+        Name = "myvpc"
+    }
+}
+
+resource "aws_subnet" "public-subnet" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = var.public_subnet_cidr
+    availability_zone = "var.public_az"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "public-subnet"
+    }
+}
+
+resource "aws_subnet" "private-subnet" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = var.private_subnet_cidr
+    availability_zone = "var.private_az"
+    tags = {
+        Name = "prrivate-subnet"
+    }
+}
+  
+  resource  "aws_internet_gateway" "myigw" {
+    vpc_id = aws_vpc.myvpc.id
+    tags = {
+        Name = "myigw"
+    }
+  }
+
+  resource "aws_eip" "nat_eip" {
+    domain = "vpc"
+    tags = {
+        Name = "nat_eip"
+    }
+  }
+
+  resource "aws_nat_gateway" "nat_gateway" {
+    subnet_id = aws_subnet.public_subnet.id
+    allocation_id = aws_eip.nat.eip.id
+    tags = {
+        Name = "nat-gateway"
+    }
+  }
+
+  resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.myvpc.id
+    route {
+        gateway_id = aws_internet_gateway.myigw.id
+        cidr_block = "0.0.0.0/0"
+    }
+  }
+
+  resource "aws_route_table_association" "public_rt_assoc" {
+    subnet_id = "aws_subnet.public_subnet.id"
+    route_table_id = "aws_route_table.public_rt.id"
+  }
+
+  resource "aws_route_table" "private_rt" {
+    vpc_id = aws_vpc.myvpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gateway.id
+    }
+  }
+
+resource "aws_route_table_association" "private_rt_assoc" {
+    subnet_id = "aws_subnet.private_subnet.id"
+    route_table_id = "aws_route_table.private_rt.id"
+}
+
+resource "aws_security_group" "my_sg" {
+    name = "my-security-group-vpc"
+    description = "my-security-group-vpc"
+    vpc_id = aws_vpc.myvpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "my-security-group-vpc"
+    }
+}
+
+resource "aws_instance" "public_instance" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
+    subnet_id = aws_subnet.public_subnet.id
+    associate_public_ip_address = true
+    user_data = file("/root/terraform32/day3/user-data.sh")
+
+    tags = {
+        Name = "public-instance"
+    }
+}
+
+resource "aws_instance" "private_instance" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    vpc_security_group_ids = [aws_security_group.my_sg.id]
+    subnet_id = aws_subnet.private-subnet.id
+    user_data = file("/root/terraform32/day3/user-data.sh")
+
+    tags = {
+        Name = "private-instance"
+    }
+}
