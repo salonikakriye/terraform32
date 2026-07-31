8f16a8786ca52b324e5d3095822b3b92b2243508
salonikakriye <vanshukakriye4@gmail.com>
2026-07-31 21:30:06 +0530

    day6

--- day6tf/moduel/vpc/output.tf
+++ day6tf/moduel/vpc/output.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-    value = aws_vpc.my_vpc.id
+    value = aws_vpc.my_vpc.id 
 }
 
 output "public_subnet_id_1" {
-    value = aws_subnet.public_subnet_1.id
+    value = aws_subnet.public_subnet_1.id 
 }
 
 output "public_subnet_id_2" {
-    value = aws_subnet.public_subnet_2.id
+    value = aws_subnet.public_subnet_2.id 
 }
 
 output "sg_id" {
-    value = aws_security_group.sg.id
+    value = aws_security_group.sg.id 
 }
